3c973fe3543b0067dd0b1f3b82cafb51f0518262
ShiharaKaumadi <eshani9544@gmail.com>
2023-06-06 16:24:25 +0530

    Create LoginForm.fxml

--- src/lk/ijse/mychat/client/controller/LoginFormController.java
+++ src/lk/ijse/mychat/client/controller/LoginFormController.java
@@ -1,4 +1,18 @@
 package lk.ijse.mychat.client.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
 public class LoginFormController {
+    public ImageView logoImg;
+    public Text loginTitle;
+    public TextField txtUsername;
+    public TextField txtHost;
+    public JFXButton btnLogin;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
 }
