1cd3654d5ae183e69c9c24c19792cd49edaf7779
ShiharaKaumadi <eshani9544@gmail.com>
2023-06-06 20:40:00 +0530

    Create btnLoginOnAction Method

--- src/lk/ijse/mychat/client/controller/LoginFormController.java
+++ src/lk/ijse/mychat/client/controller/LoginFormController.java
@@ -2,11 +2,16 @@ package lk.ijse.mychat.client.controller;
 
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public ImageView logoImg;
@@ -16,7 +21,19 @@ public class LoginFormController {
     public JFXButton btnLogin;
     public AnchorPane loginPage;
     public Pane content;
+    private String host;
+    private String username;
 
-    public void btnLoginOnAction(ActionEvent actionEvent) {
+    public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
+        if (!txtUsername.getText().equals("")){
+            username = txtUsername.getText();
+            if (!txtHost.getText().equals("")){
+                host=txtHost.getText();
+                Stage stage = new Stage();
+                stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("src/lk/ijse/mychat/client/views/ChatUIForm.fxml"))));
+                stage.centerOnScreen();
+                stage.show();
+            }
+        }
     }
 }
