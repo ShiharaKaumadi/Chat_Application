d511ff399ed634405ff7fc63794bb57eef1b227f
ShiharaKaumadi <eshani9544@gmail.com>
2023-06-06 21:22:19 +0530

    Create ServerForm.fxml

--- src/lk/ijse/mychat/server/ServerInitializer.java
+++ src/lk/ijse/mychat/server/ServerInitializer.java
@@ -1,7 +1,11 @@
 package lk.ijse.mychat.server;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ServerInitializer extends Application {
 
@@ -10,7 +14,10 @@ public class ServerInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene
+                (new Scene(FXMLLoader.load(getClass().getResource("views/ServerForm.fxml"))));
+        primaryStage.show();
 
     }
 }
---
+++ src/lk/ijse/mychat/server/controller/ServerFormController.java
@@ -0,0 +1,25 @@
+package lk.ijse.mychat.server.controller;
+
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class ServerFormController {
+    public AnchorPane serverPane;
+    public TextArea txtArea;
+    public TextField txtMessage;
+    public ImageView emoji;
+    public ImageView file;
+    public ImageView btnSend;
+
+    public void emojiOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void selectFileOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void sendOnAction(MouseEvent mouseEvent) {
+    }
+}
