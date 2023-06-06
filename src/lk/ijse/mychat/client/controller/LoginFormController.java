05958a549d45c6d304a1add3d6ef0da3dc22f8d1
ShiharaKaumadi <eshani9544@gmail.com>
2023-06-06 20:26:33 +0530

    Added Styles to Login Form

--- src/lk/ijse/mychat/client/controller/LoginFormController.java
+++ src/lk/ijse/mychat/client/controller/LoginFormController.java
@@ -4,6 +4,8 @@ import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
 public class LoginFormController {
@@ -12,6 +14,8 @@ public class LoginFormController {
     public TextField txtUsername;
     public TextField txtHost;
     public JFXButton btnLogin;
+    public AnchorPane loginPage;
+    public Pane content;
 
     public void btnLoginOnAction(ActionEvent actionEvent) {
     }
