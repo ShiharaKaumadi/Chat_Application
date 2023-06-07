31f678009f807d61843bc06b70ad5fe08c0bcf42
ShiharaKaumadi <eshani9544@gmail.com>
2023-06-07 15:44:22 +0530

    Display message on pane

--- src/lk/ijse/mychat/client/ChatUIFormController.java
+++ src/lk/ijse/mychat/client/ChatUIFormController.java
@@ -1,10 +1,16 @@
 package lk.ijse.mychat.client;
 
-import javafx.scene.control.TextArea;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,12 +21,14 @@ import java.net.Socket;
 public class ChatUIFormController {
     public Pane paneHeader;
     public ImageView call;
-    public TextArea txtClientArea;
+    public Pane txtClientArea;
     public ImageView btnSend;
     public TextField txtClientMessage;
     public ImageView emoji;
     public ImageView file;
+    public Label lblUser;
     Socket socket =null;
+
 
     public  void initialize() throws IOException {
         socket = new Socket("localhost",5000);
@@ -36,8 +44,23 @@ public class ChatUIFormController {
     public void sendClientMsgOnAction(MouseEvent mouseEvent) throws IOException {
         PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
         printWriter.println(txtClientMessage.getText());
-        printWriter.write(txtClientArea.getSelectedText());
         printWriter.flush();
+        String clientMessage = txtClientMessage.getText();
+        if(!clientMessage.isEmpty()){
+           Pane pane = new Pane();
+            pane.setPadding(new Insets(10,5,5,10));
+            pane.setLayoutX(320);
+            Text text = new Text(clientMessage);
+            TextFlow textFlow = new TextFlow(text);
+            textFlow.setStyle("-fx-background-color: #7E308E;"+"-fx-background-radius: 10px");
+            textFlow.setPadding(new Insets(10,10,10,10));
+            text.setFill(Color.color(1,1,1));
+            pane.getChildren().add(textFlow);
+            txtClientArea.getChildren().add(pane);
+            txtClientMessage.clear();
+            pane.setPadding(new Insets(0,0,30,0)); ;// Set horizontal gap
+
+        }
     }
 
     public void fileClickOnAction(MouseEvent mouseEvent) {
--- src/lk/ijse/mychat/server/ServerFormController.java
+++ src/lk/ijse/mychat/server/ServerFormController.java
@@ -1,10 +1,16 @@
 package lk.ijse.mychat.server;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,7 +21,7 @@ import java.net.Socket;
 
 public class ServerFormController {
     public AnchorPane serverPane;
-    public TextArea txtArea;
+    public Pane txtArea;
     public TextField txtMessage;
     public ImageView emoji;
     public ImageView file;
@@ -68,6 +74,24 @@ public class ServerFormController {
         PrintWriter printWriter = new PrintWriter(accept.getOutputStream());
         printWriter.println(txtMessage.getText());
         printWriter.flush();
+        String clientMessage = txtMessage.getText();
+        if (!clientMessage.isEmpty()){
+            Pane pane = new Pane();
+            pane.setPadding(new Insets(10,5,5,10));
+            pane.setLayoutX(10);
+            Text text = new Text(clientMessage);
+            TextFlow textFlow = new TextFlow(text);
+            textFlow.setStyle("-fx-background-color: #0c78b2;"+"-fx-background-radius: 10px");
+            textFlow.setPadding(new Insets(10,10,10,10));
+            text.setFill(Color.color(1,1,1));
+            pane.getChildren().add(textFlow);
+            txtArea.getChildren().add(pane);
+            txtMessage.clear();
+            pane.setPadding(new Insets(0,0,30,0)); ;// Set horizontal gap
+
+
+
+        }
 
     }
 }
