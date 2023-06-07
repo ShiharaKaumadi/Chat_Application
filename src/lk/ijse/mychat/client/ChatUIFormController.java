9f28ded11984f1020274ba7c28bcb65088caa9d3
ShiharaKaumadi <eshani9544@gmail.com>
2023-06-07 19:40:53 +0530

    Exchange Messages between Clients

--- src/lk/ijse/mychat/client/ChatUIFormController.java
+++ src/lk/ijse/mychat/client/ChatUIFormController.java
@@ -1,17 +1,19 @@
 package lk.ijse.mychat.client;
 
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import java.io.*;
 import java.net.Socket;
@@ -54,6 +56,7 @@ public class ChatUIFormController {
             text.setFill(Color.color(1,1,1));
             pane.getChildren().add(textFlow);
             txtClientArea.getChildren().add(pane);
+            client.sendMessageToServer(username+" : "+clientMessage);
             txtClientMessage.clear();
             pane.setPadding(new Insets(0,0,30,0)); ;// Set horizontal gap
 
@@ -61,6 +64,23 @@ public class ChatUIFormController {
     }
 
     public void fileClickOnAction(MouseEvent mouseEvent) {
+        Stage stage = new Stage();
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+
+            HBox hBox =  new HBox();
+            hBox.setAlignment(Pos.CENTER_RIGHT );
+            hBox.setPadding(new Insets(5,10,5,10));
+            Image image = new Image("file:"+file.getAbsolutePath());
+            ImageView imageView = new ImageView(image);
+            imageView.setFitWidth(100);
+            imageView.setFitHeight(100);
+            hBox.getChildren().add(imageView);
+            txtClientArea.getChildren().add(hBox);
+            client.sendFileToServer(file, username);
+            System.out.println("send "+file.getName());
+        }
     }
 
     public void emojiOnAction(MouseEvent mouseEvent) throws IOException {
@@ -68,8 +88,45 @@ public class ChatUIFormController {
     }
 
     public static void addLabel(String messageFromClient, AnchorPane anchorPane) {
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER_LEFT);
+        hBox.setPadding(new Insets(5,10,5,10));
+
+        Text text = new Text(messageFromClient);
+        TextFlow textFlow = new TextFlow(text);
+        textFlow.setStyle("-fx-background-color: rgb(198,194,194);"+"-fx-background-radius: 10px");
+        textFlow.setPadding(new Insets(5,10,5,10));
+        hBox.getChildren().add(textFlow);
+
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                anchorPane.getChildren().add(hBox);
+            }
+        });
     }
 
     public static void addImage(File fileToDownload, AnchorPane anchorPane, String senderName) {
+        HBox hBox = new HBox();
+        VBox vBox1 = new VBox();
+        vBox1.setStyle("-fx-background-color: rgb(198,194,194);"+"-fx-background-radius: 5px");
+        hBox.setAlignment(Pos.CENTER_LEFT);
+        hBox.setPadding(new Insets(5,10,5,10));
+        Text text = new Text(senderName);
+        vBox1.getChildren().add(text);
+        Image image = new Image("file:"+fileToDownload.getAbsolutePath());
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(100);
+        imageView.setFitHeight(100);
+        vBox1.setPadding(new Insets(5,10,5,10));
+        vBox1.getChildren().add(imageView);
+        hBox.getChildren().add(vBox1);
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                anchorPane.getChildren().add(hBox);
+            }
+        });
     }
+
 }
--- src/lk/ijse/mychat/server/HandlingEvents.java
+++ src/lk/ijse/mychat/server/HandlingEvents.java
@@ -4,89 +4,153 @@ import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 /*managing client connections and handling message communication between multiple clients*/
-public class HandlingEvents implements Runnable{
+public class HandlingEvents implements Runnable {
     public static ArrayList<HandlingEvents> handlingEvents = new ArrayList<>();//represents connected clients.
     //handling the network connection and client information-->socket, bufferReader,bufferWriter,client
-    private Socket socket;
+    public Socket socket;
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
     private String client;
 
     public HandlingEvents(Socket socket, String client) {// the client's socket connection and client name
-        try{
+        try {
             this.socket = socket;
             // input and output streams initialized for socket connection
-            this.bufferedWriter=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            this.bufferedReader= new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            this.client=client;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.client = client;
             //add connected clients arraylist
             handlingEvents.add(this);
             //sends a message to all connected clients except the sender
-            sendMessage(client+" connected!");
-        }catch (IOException e ){
-            closeAll(socket,bufferedWriter,bufferedReader);
+            sendMessage(client + " connected!");
+        } catch (IOException e) {
+            closeAll(socket, bufferedWriter, bufferedReader);
         }
     }
 
     // Close all resources (socket, bufferedWriter, bufferedReader)
     private void closeAll(Socket socket, BufferedWriter bufferedWriter, BufferedReader bufferedReader) {
         removeClient();
-        try{
-            if(bufferedReader!=null){
+        try {
+            if (bufferedReader != null) {
                 bufferedReader.close();
             }
-            if (bufferedWriter!=null){
+            if (bufferedWriter != null) {
                 bufferedWriter.close();
             }
-            if(socket!=null){
+            if (socket != null) {
                 socket.close();
             }
-        }catch (IOException e ){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     // Send message to all connected clients except the sender
     private void sendMessage(String message) {
         //iterates through the arraylist list and writes the message to the bufferedWriter of each connected client.
-        for(HandlingEvents handle : handlingEvents){
-            try{
-                if (!handle.client.equals(client)){
+        for (HandlingEvents handle : handlingEvents) {
+            try {
+                if (!handle.client.equals(client)) {
                     handle.bufferedWriter.write(message);
                     handle.bufferedWriter.newLine();
                     handle.bufferedWriter.flush();
                 }
-            }catch (IOException e){
+            } catch (IOException e) {
                 //If any exceptions occur during socket operations or message sending, close the socket
-                closeAll(socket,bufferedWriter,bufferedReader);
+                closeAll(socket, bufferedWriter, bufferedReader);
             }
         }
     }
 
     @Override
     public void run() {
-        String  clientMessage;
+        String clientMessage;
 
-        while(socket.isConnected()){
-            try{
-                clientMessage=bufferedReader.readLine();
-                //System.out.println("run msg "+messageFromClient);
-                if(clientMessage.split(":")[0].equals("FILE")){
-
-                }else{
+        while (socket.isConnected()) {
+            try {
+                clientMessage = bufferedReader.readLine();
+                if (clientMessage.split(":")[0].equals("FILE")) {
+                    downloadFile(clientMessage.split(":")[1]);
+                } else {
                     sendMessage(clientMessage);
                 }
 
-            }catch (IOException e ){
+            } catch (IOException e) {
                 System.out.println(e.getMessage());
-                closeAll(socket,bufferedWriter,bufferedReader);
+                closeAll(socket, bufferedWriter, bufferedReader);
                 break;
             }
         }
 
     }
 
-    public void removeClient(){
-        handlingEvents.remove(this);
-        sendMessage(client + " left");
+    private void downloadFile(String name) throws IOException {
+        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+        int fileNameLength = dataInputStream.readInt();
+        if (fileNameLength > 0) {
+            System.out.println(fileNameLength);
+            byte[] fileNameByte = new byte[fileNameLength];
+            dataInputStream.readFully(fileNameByte, 0, fileNameByte.length);
+            String fileName = new String(fileNameByte);
+            System.out.println(fileName);
+
+            int fileContentLength = dataInputStream.readInt();
+            if (fileContentLength > 0) {
+                byte[] fileContentByte = new byte[fileContentLength];
+                dataInputStream.readFully(fileContentByte, 0, fileContentLength);
+
+                File fileToDownload = new File(fileName);
+                try {
+                    FileOutputStream fileOutputStream = new FileOutputStream(fileToDownload);
+                    fileOutputStream.write(fileContentByte);
+                    fileOutputStream.close();
+                    System.out.println("download");
+
+                    sendFile(fileToDownload, name);
+                } catch (IOException e) {
+                    System.out.println("download " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        }
     }
+
+        public void removeClient () {
+            handlingEvents.remove(this);
+            sendMessage(client + " left");
+        }
+
+
+        private void sendFile (File file, String name){
+            System.out.println(name+" image from");
+            for(HandlingEvents clients : handlingEvents){
+                try{
+                    if (!clients.client.equals(name)){
+                        FileInputStream fileInputStream = new FileInputStream(file.getAbsolutePath());
+                        DataOutputStream dataOutputStream = new DataOutputStream(clients.socket.getOutputStream());
+                        PrintWriter printWriter = new PrintWriter(clients.socket.getOutputStream());
+                        printWriter.println("FILE:"+name);
+                        printWriter.flush();
+
+                        String fileName = file.getName();
+                        byte[] fileNameBytes = fileName.getBytes();
+                        byte[] fileContentBytes = new byte[(int)file.length()];
+                        fileInputStream.read(fileContentBytes);
+
+                        dataOutputStream.writeInt(fileNameBytes.length);
+                        dataOutputStream.write(fileNameBytes);
+                        dataOutputStream.writeInt(fileContentBytes.length);
+                        dataOutputStream.write(fileContentBytes);
+                        dataOutputStream.flush();
+                        System.out.println(clients.client+" to broadcast "+fileName);
+
+                    }
+                }catch (IOException e){
+                    System.out.println("upload "+e.getMessage());
+                    closeAll(socket,bufferedWriter,bufferedReader);
+                }
+            }
+        }
+
 }
